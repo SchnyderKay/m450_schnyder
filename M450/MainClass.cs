d6f31f1d727a49c810f44611507502be5c5c62b7
Kay Schnyder <schnyderkay@gmail.com>
2023-08-24 10:56:05 +0200

    grundlagen

---
+++ M450/MainClass.cs
@@ -0,0 +1,13 @@
+﻿using M450.grundlagen;
+using System;
+
+namespace M450
+{
+    public class MainClass
+    { static void Main(string[] args)
+        {
+            PricingTest pricingTest = new PricingTest();
+            pricingTest.TestPrice();
+        }
+    }
+}
---
+++ M450/grundlagen/PricingTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M450.grundlagen
+{
+    [TestClass]
+    internal class PricingTest
+    {
+        [TestMethod]
+        internal void TestPrice()
+        {
+            Pricing pricing = new Pricing();
+            Assert.IsNotNull(pricing);
+
+            Assert.AreEqual(pricing.calculatePrice(5.5, 12.3, 40, 5, 20), 48.7);
+            Assert.AreEqual(pricing.calculatePrice(50.6, 22.7, 23.34, 12, 7.9), 90.3086);
+            Assert.AreEqual(pricing.calculatePrice(0, 94.3, 4.3, 0, 5.3), 98.3721);
+        }
+    }
+}
