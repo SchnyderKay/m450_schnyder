e8d9afde8f2c67375b8261bba1c0c1f8954b8fd2
Kay Schnyder <schnyderkay@gmail.com>
2023-09-14 11:10:25 +0200

    14.09.2023

--- UnitTesting/Augabe4/test/java/ch/schule/bank/junit5/BookingTests.java
+++ UnitTesting/Augabe4/test/java/ch/schule/bank/junit5/BookingTests.java
@@ -4,7 +4,6 @@ import ch.schule.Booking;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 
 /**
